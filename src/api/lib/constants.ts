e306d7e04850d452ef60079656104b022db2d419
Olufemi Okikioluwa <101170232+samlufi@users.noreply.github.com>
2022-05-01 00:58:06 +0100

    restored constants

--- src/api/lib/constants.ts
+++ src/api/lib/constants.ts
@@ -1,5 +1,8 @@
 export const corsWhitelist: string[] = ['*']
 
+export const { NODE_ENV, PORT } = process.env
+
 export const constants = {
   SUCCESSFUL: 'successful',
+  COUNTRIES_BY_CODES_URL: 'https://restcountries.eu/rest/v2/alpha?codes=',
 }
